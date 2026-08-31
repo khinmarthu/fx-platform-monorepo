3916e7e92ab4dfb3ac5ed2725ed4c9caf1bdfa7f
Khin Mar Thu <kmt.khinmarthu@gmail.com>
2026-08-31 23:44:41 +0800

    add EventBus to communicate between isolated micro frontends

---
+++ packages/rx-engine/src/EventBus.ts
@@ -0,0 +1,33 @@
+import { Observable, Subject } from "rxjs";
+
+export interface TradeEvent {
+    type: "ORDER_EXECUTED";
+    payload: {
+        orderId: string;
+        symbol: string;
+        side: 'BUY' | 'SELL';
+        amount: number;
+        executedPrice: number;
+        timestamp: number;
+    }
+}
+
+export class EventBus {
+    private eventSubject$ = new Subject<TradeEvent>();
+
+    /**
+     * Publish a cross-MFE event.
+     */
+    public emit(trade: TradeEvent): void {
+        this.eventSubject$.next(trade);
+    }
+
+    /**
+     * Listen for cross-MFE events.
+     */
+    public onEvent(): Observable<TradeEvent> {
+        return this.eventSubject$.asObservable();
+    }
+}
+
+export const eventBus = new EventBus();
