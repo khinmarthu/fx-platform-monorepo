ab4cbb97757a156caa03f240f2ec5304127c1d50
Khin Mar Thu <kmt.khinmarthu@gmail.com>
2026-09-04 19:42:30 +0800

    Re-export common RxJS utilities so MFEs don't need a direct 'rxjs' dependenc

--- packages/rx-engine/src/index.ts
+++ packages/rx-engine/src/index.ts
@@ -1,3 +1,7 @@
 export * from './EventBus';
 export * from './FXStreamService';
 export * from './fxWorker';
+
+// To prevents version mismatches between MFEs and rx-engine
+// Re-export common RxJS utilities so MFEs don't need a direct 'rxjs' dependency
+export { Subject, Observable, takeUntil, filter, map } from 'rxjs';
