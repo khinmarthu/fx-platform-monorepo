55663887b23383ab0dbca8607ce26ce215075c4b
Khin Mar Thu <kmt.khinmarthu@gmail.com>
2026-09-23 18:50:05 +0800

    update vite config for blotter

--- apps/blotter-mfe/vite.config.ts
+++ apps/blotter-mfe/vite.config.ts
@@ -27,9 +27,14 @@ export default defineConfig({
     // Crucial for development: ensures the host app can cross-origin fetch assets from this port
     cors: true,
   },
+  preview: {
+    port: 3002, // 3002 for blotter-mfe
+    cors: true, // Enables CORS headers during `pnpm preview`
+    strictPort: true,
+  },
   build: {
     // Required for Module Federation 2.0 to support top-level await initialization
-    target: 'esnext',
+    target: 'es2022',
     // Optional but recommended for MFEs: ensures preloaded module strategies do not clash
     modulePreload: false,
   },
